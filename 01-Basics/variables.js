c534966ee6cbeee56b97ea13528c4306288d2251
uzai-rriaz1 <uzairriaz1296@gmail.com>
2025-07-08 12:18:27 +0000

    Javascript

---
+++ 01-Basics/variables.js
@@ -0,0 +1,13 @@
+const name  = "uzair";
+
+let rollno = 49;
+
+var classname = "BSIS";
+
+console.table([name, rollno, classname]);
+
+
+// const name = "ammar"    we cant change a const variable value
+
+/* we can change the value of let and var variables but we should not use var to declare variables
+BECAUSE ITS has block level scope which creates problems so we always use let to declare.*/
